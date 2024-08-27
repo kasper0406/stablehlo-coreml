Implement the topk operation and fix broadcasting

stablehlo_coreml/converter.py
@@ -3,6 +3,9 @@ from coremltools.converters.mil import mil
 from coremltools.converters.mil.mil import Builder as mb
 from coremltools.converters.mil.mil import Function, Program, types
 from coremltools.converters.mil._deployment_compatibility import AvailableTarget
+from coremltools.converters.mil.mil.ops.defs._utils import (
+    promote_input_dtypes,
+)
 from .utils import index_by_slices, update_tensor_by_slice, iterate_indexes_in_shapes
 
 from jaxlib.mlir import ir
@@ -11,17 +14,17 @@ from jaxlib.mlir.dialects.stablehlo import (
     AddOp, SubtractOp, MulOp, DivOp, NegOp, SignOp, AbsOp, ExpOp, Log1pOp, SqrtOp,
     ConstantOp, DotGeneralOp, ReshapeOp, BroadcastInDimOp, WhileOp, CompareOp,
     ConvertOp, SelectOp, DynamicSliceOp, ReturnOp, ConvolutionOp, MaxOp, RsqrtOp,
-    TanhOp, ConcatenateOp, TransposeOp, DynamicUpdateSliceOp, SliceOp, CustomCallOp
+    TanhOp, ConcatenateOp, TransposeOp, DynamicUpdateSliceOp, SliceOp, CustomCallOp,
+    IotaOp
 )
 from jaxlib.mlir.dialects.mhlo import (TopKOp)
-from jax._src.lib.mlir.dialects import hlo, mhlo
+from jax._src.lib.mlir.dialects import hlo
 
 import numpy as np
 
 from typing import List, Optional
 import inspect
 from functools import partial, reduce
-import operator
 
 
 def convert(module, minimum_deployment_target: AvailableTarget):
@@ -299,6 +302,7 @@ class StableHloConverter(metaclass=StableHloOpsRegistry):
     @register_stablehlo_op
     def op_constant(self, context: TranscriptionContext, op: ConstantOp):
         constant = np.array(op.value)
+        constant = np.reshape(constant, op.result.type.shape)
         context.add_variable(op.result.get_name(), constant)
 
     @register_stablehlo_op
@@ -415,22 +419,20 @@ class StableHloConverter(metaclass=StableHloOpsRegistry):
 
     @register_stablehlo_op
     def op_broadcast_in_dim(self, context: TranscriptionContext, op: BroadcastInDimOp):
-        # TODO(knielsen): Consider if this is actually correct!
-        # CoreML seems to auto-broadcast along the lines of numpy. Therefore this
-        # explicit broadcasting op is not necessary.
         x = context[op.operand.get_name()]
 
-        # We handle one special case where the broadcast functions as a reshape
-        op_elements = reduce(operator.mul, op.result.type.shape, 1)
-        x_elements = reduce(operator.mul, x.shape, 1)
-        if op_elements == x_elements:
-            # We know that the only possibility is for data to be added, so this is likely a reshape
-            x = mb.reshape(x=x, shape=op.result.type.shape)
+        reshaped_operand_shape = [1] * len(op.result.type.shape)
+        for i, op_shape in enumerate(op.operand.type.shape):
+            result_idx = op.broadcast_dimensions[i]
+            reshaped_operand_shape[result_idx] = op_shape
 
-        # Another special case. If we are broadcasting a constant in all directions, just change the shape
-        if len(op.broadcast_dimensions) == 0 and len(x.shape) == 0:
-            dtype = types.nptype_from_builtin(self.__resolve_type(x))
-            x = mb.mul(x=x, y=np.ones(op.result.type.shape, dtype=dtype))
+        x = mb.reshape(x=x, shape=reshaped_operand_shape)
+        for result_dim, current_shape in enumerate(reshaped_operand_shape):
+            if current_shape != op.result.type.shape[result_dim]:
+                assert current_shape == 1
+                # Replicate data along dimension `dim` until the result dimension is filled up
+                values = [x] * op.result.type.shape[result_dim]
+                x = mb.concat(values=values, axis=result_dim)
 
         context.add_variable(op.result.get_name(), x)
 
@@ -676,8 +678,17 @@ class StableHloConverter(metaclass=StableHloOpsRegistry):
     @register_stablehlo_op
     def op_concatenate(self, context: TranscriptionContext, op: ConcatenateOp):
         values = [context[input.get_name()] for input in op.inputs]
+        values = promote_input_dtypes(values)
         mil_res = mb.concat(values=values, axis=op.dimension.value)
         context.add_variable(op.result.get_name(), mil_res)
+
+    @register_stablehlo_op
+    def op_iota(self, context: TranscriptionContext, op: IotaOp):
+        iota_dim = int(op.iota_dimension)
+        tensor_shape = op.result.type.shape
+        vec_shape = [tensor_shape[dim] if dim == iota_dim else 1 for dim in range(len(tensor_shape))]
+        res = np.reshape(np.arange(tensor_shape[iota_dim]), vec_shape) * np.ones(tensor_shape)
+        context.add_variable(op.result.get_name(), res)
 
     @register_stablehlo_op
     def op_custom_call(self, context: TranscriptionContext, op: CustomCallOp):

tests/test_jax.py
@@ -85,13 +85,8 @@ def test_tensor_multiplication():
 
 
 def test_topk():
-    def topk_jvp(primals, tangents, k: int):
-        topk = partial(jax.lax.top_k, k=k)
-        return jax.jvp(topk, primals, tangents)
-
     input_shape = (3, 5, 10)
     run_and_compare(partial(jax.lax.top_k, k=3), (jnp.zeros(input_shape),))
-    # run_and_compare(partial(topk_jvp, k=3), ([jnp.zeros(input_shape)], [jnp.zeros(input_shape)]))
 
 
 def jax_export(jax_func, input_spec):
@@ -188,7 +183,19 @@ def run_and_compare(jax_func, input_spec, max_complexity: int = 10_000):
             "max allowed complexity is {max_complexity}"
         )
 
-    cml_model = ct.convert(mil_program, source="milinternal", minimum_deployment_target=ct.target.iOS18)
+    pipeline = ct.PassPipeline.DEFAULT
+    # We temporarily avoid fp16 conversions in tests because of https://github.com/apple/coremltools/issues/2324
+    passes_to_remove = [
+         'common::add_fp16_cast'
+    ]
+    pipeline.remove_passes(passes_to_remove)
+
+    cml_model = ct.convert(
+        mil_program,
+        source="milinternal",
+        minimum_deployment_target=ct.target.iOS18,
+        pass_pipeline=pipeline,
+    )
 
     # Generate random inputs that matches cml_model input spec
     cml_input_key_values = {}